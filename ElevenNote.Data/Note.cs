ccf8d664024247ac3dc9553ab44aa2db00a12595
Merideth Bush <meridethbush@gmail.com>
2018-08-09 11:48:02 -0400

    added files

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace ElevenNote.Data
+{
+    public class Note
+    {
+        public int NoteID { get; set; }
+        public Guid OwnerID { get; set; }
+        public string Content { get; set; }
+        public string Content { get; set; }
+    }
+}
