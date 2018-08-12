daa8874d6f62e55c4c6713a58def2bf22efb4fc0
Merideth Bush <meridethbush@gmail.com>
2018-08-12 17:46:20 -0400

    updated through part 12: stars

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -48,6 +48,7 @@ namespace ElevenNote.Services
                                 {
                                     NoteID = e.NoteID,
                                     Title = e.Title,
+                                    IsStarred = e.IsStarred,
                                     CreatedUtc = e.CreatedUtc
                                 });
                 return query.ToArray();
@@ -86,6 +87,7 @@ namespace ElevenNote.Services
                 entity.Title = model.Title;
                 entity.Content = model.Content;
                 entity.ModifiedUtc = DateTimeOffset.UtcNow;
+                entity.IsStarred = model.IsStarred;
 
                 return ctx.SaveChanges() == 1;
             }
